322c952a2636939eba2b607241267e5f3600c06a
ztzrk <ralif877@gmail.com>
2025-07-02 20:15:07 +0700

    feat: add IconMap component for managing and rendering icons

---
+++ src/components/shared/IconMap.tsx
@@ -0,0 +1,59 @@
+import {
+    FaBaseballBatBall,
+    FaBicycle,
+    FaBowlingBall,
+    FaDumbbell,
+    FaFootball,
+    FaFutbol,
+    FaGolfBallTee,
+    FaPersonBiking,
+    FaPersonRunning,
+    FaPersonSkating,
+    FaPersonSkiing,
+    FaPersonSnowboarding,
+    FaPersonSwimming,
+    FaTableTennisPaddleBall,
+    FaVolleyball,
+} from "react-icons/fa6";
+import { GiShuttlecock } from "react-icons/gi";
+import { HelpCircle } from "lucide-react";
+import React from "react";
+
+export const iconMap: Record<string, React.ComponentType<any>> = {
+    futbol: FaFutbol,
+    tableTennis: FaTableTennisPaddleBall,
+    bicycle: FaBicycle,
+    dumbbell: FaDumbbell,
+    volleyball: FaVolleyball,
+    swimming: FaPersonSwimming,
+    running: FaPersonRunning,
+    snowboarding: FaPersonSnowboarding,
+    biking: FaPersonBiking,
+    skiing: FaPersonSkiing,
+    skating: FaPersonSkating,
+    golf: FaGolfBallTee,
+    football: FaFootball,
+    bowlingBall: FaBowlingBall,
+    baseballBatBall: FaBaseballBatBall,
+    shuttlecock: GiShuttlecock,
+};
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+    name?: string;
+}
+
+export function Icon({ name, ...props }: IconProps) {
+    if (!name) {
+        return <HelpCircle {...props} />;
+    }
+
+    const IconComponent = iconMap[name];
+
+    if (!IconComponent) {
+        return <HelpCircle {...props} />;
+    }
+
+    return <IconComponent {...props} />;
+}
+
+export const availableIcons = Object.keys(iconMap);
