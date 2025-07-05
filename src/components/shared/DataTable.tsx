15db622522d6a87b1cf869582d66bc7e49cf865b
ztzrk <ralif877@gmail.com>
2025-07-05 20:55:15 +0700

    feat: implement DataTable component for displaying and managing data with selection and deletion functionality

---
+++ src/components/shared/DataTable.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    getSortedRowModel,
+    SortingState,
+    RowSelectionState,
+} from "@tanstack/react-table";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import UserService from "@/services/user.service";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import ConfirmationDialog from "@/components/shared/ConfirmationDialog";
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+    const router = useRouter();
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        onRowSelectionChange: setRowSelection,
+        state: {
+            sorting,
+            rowSelection,
+        },
+    });
+
+    const handleDeleteSelected = async () => {
+        try {
+            const selectedIds = table
+                .getFilteredSelectedRowModel()
+                .rows.map((row) => (row.original as any).id);
+
+            await UserService.deleteMultipleUsers(selectedIds);
+            toast.success(
+                `${selectedIds.length} user(s) deleted successfully.`
+            );
+            router.refresh();
+            table.resetRowSelection();
+        } catch (error) {
+            toast.error("Failed to delete selected users.");
+        }
+    };
+
+    return (
+        <div>
+            <div className="flex items-center py-4">
+                {Object.keys(rowSelection).length > 0 && (
+                    <ConfirmationDialog
+                        trigger={
+                            <Button variant="destructive" className="mr-2">
+                                Delete Selected
+                            </Button>
+                        }
+                        title="Delete Confirmation"
+                        description="Are you sure you want to delete the selected users?"
+                        onConfirm={handleDeleteSelected}
+                        action="Delete"
+                    />
+                )}
+            </div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext()
+                                              )}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={
+                                        row.getIsSelected() && "selected"
+                                    }
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    );
+}
