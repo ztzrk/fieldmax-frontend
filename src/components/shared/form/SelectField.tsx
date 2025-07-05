9b76d3070508cd1ab73d298f97cf1db62c759c38
ztzrk <ralif877@gmail.com>
2025-07-05 20:55:21 +0700

    feat: add SelectField component for form handling with react-hook-form integration

---
+++ src/components/shared/form/SelectField.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Control } from "react-hook-form";
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+interface SelectFieldProps {
+    control: Control<any>;
+    name: string;
+    label: string;
+    placeholder?: string;
+    options: { value: string; label: string }[];
+}
+
+export function SelectField({
+    control,
+    name,
+    label,
+    placeholder,
+    options,
+}: SelectFieldProps) {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel>{label}</FormLabel>
+                    <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        value={field.value}
+                    >
+                        <FormControl>
+                            <SelectTrigger>
+                                <SelectValue placeholder={placeholder} />
+                            </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                            {options.map((option) => (
+                                <SelectItem
+                                    key={option.value}
+                                    value={option.value}
+                                >
+                                    {option.label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+}
