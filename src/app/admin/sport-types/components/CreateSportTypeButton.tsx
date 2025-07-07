9c2ddcb6c036a56cef52aa83ec2af651dd44c23d
ztzrk <ralif877@gmail.com>
2025-07-07 20:45:38 +0700

    feat: add CreateSportTypeButton component for creating new sport types

---
+++ src/app/admin/sport-types/components/CreateSportTypeButton.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { useCreateSportType } from "@/hooks/useSportTypes";
+import React from "react";
+import { toast } from "sonner";
+import { SportTypeForm } from "./SportTypeForm";
+
+function CreateSportTypeButton() {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const { mutate: createSportType, isPending } = useCreateSportType();
+
+    const handleSubmit = async (data: any) => {
+        createSportType(data, {
+            onSuccess: () => {
+                toast.success(`${data.name} created successfully`);
+                setIsOpen(false);
+            },
+            onError: (error) => {
+                toast.error(
+                    `Failed to create ${data.name} error: ${error.message}`
+                );
+            },
+        });
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button>Create Sport Type</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Create New Sport Type</DialogTitle>
+                </DialogHeader>
+                <SportTypeForm onSubmit={handleSubmit} isPending={isPending} />
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default CreateSportTypeButton;
