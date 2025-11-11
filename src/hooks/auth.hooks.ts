6ec3903401f8b220d6fbaaf18ba14fb88d130785
ztzrk <ralif877@gmail.com>
2025-11-11 18:23:43 +0800

    refactor: enhance useLogin hook by improving error handling and type definitions

--- src/hooks/auth.hooks.ts
+++ src/hooks/auth.hooks.ts
@@ -1,18 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import AuthService from "@/services/auth.service";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { useAuth } from "@/context/AuthContext";
+import { useAuth, User } from "@/context/AuthContext";
+import { AxiosError } from "axios";
 
 export function useLogin() {
+    const queryClient = useQueryClient();
     const router = useRouter();
-    const { login: setAuthUser } = useAuth();
+    const { login } = useAuth();
 
     return useMutation({
-        mutationFn: (credentials: { email: string; password: string }) =>
-            AuthService.login(credentials),
-        onSuccess: (user) => {
-            setAuthUser(user);
+        mutationFn: (credentials: any) => AuthService.login(credentials),
+        onSuccess: (user: User) => {
+            login(user);
 
             toast.success("Login berhasil!", {
                 description: `Selamat datang kembali, ${user.fullName}.`,
@@ -27,10 +28,28 @@ export function useLogin() {
 
             router.push(targetDashboard);
         },
-        onError: () => {
-            toast.error("Login Gagal", {
-                description: "Silakan periksa kembali email dan password Anda.",
-            });
+        onError: (error: AxiosError) => {
+            if (error.response) {
+                if (error.response.status === 401) {
+                    toast.error("Login Gagal", {
+                        description:
+                            "Email atau password yang Anda masukkan salah.",
+                    });
+                } else {
+                    toast.error("Server Error", {
+                        description: "Terjadi kesalahan di server kami.",
+                    });
+                }
+            } else if (error.request) {
+                toast.error("Tidak Terhubung", {
+                    description:
+                        "Tidak dapat terhubung ke server. Silakan periksa koneksi Anda.",
+                });
+            } else {
+                toast.error("Terjadi Kesalahan", {
+                    description: error.message,
+                });
+            }
         },
     });
 }
